f15317723310ea3dfd641827a9e968a9c6f9c63e
E0min <bbok4yo@gmail.com>
2022-07-27 21:09:57 +0900

    commit

---
+++ JavaProgramming_Lab_2/src/Lab_2.java
@@ -0,0 +1,10 @@
+public class Lab_2 {
+    public static void PeriodicElement(int number, String symbol, String name, double weight){
+        System.out.println("PeriodicElement => [number = "+number+", symbol = "+ symbol +", name = "+name+", weight = "+weight+ "]");
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- JavaProgramming_Lab_2/src/UserInput.java
+++ JavaProgramming_Lab_2/src/UserInput.java
@@ -5,15 +5,18 @@ public class UserInput {
     private static Scanner input = new Scanner(System.in);
     public static double getDouble(){ return Double.parseDouble(input.nextLine());}
     public static int getInteger(){ return Integer.parseInt(input.nextLine());}
-    public static int getIntegerBetween(int min. int max){
-
+    public static int getIntegerBetween(int min, int max){
+ // 최솟값, 최댓값 범위를 정해주기
+        return 0;
     }
 
     public static boolean getExitKey(){
         System.out.print("press q-key to exit or enter-key to continue: ");
         String s = input.nextLine();
-        if(s.contentEquals("q")) return true;
-        else return false;
+        if(s.contentEquals("q"))
+            return true;
+        else
+            return false;
     }
 
 }
